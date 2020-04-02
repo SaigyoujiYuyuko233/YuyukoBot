1e53d1384bd8ca6beaca12181604606a37789dde
SaigyoujiYuyuko <HGK-SaigyoujiYuyuko@outlook.com>
2020-04-02 09:16:55 +0800

    测试版本号

---
+++ src/main/java/network/meikai/mc/uuzselfplaybot/GlobalVars.java
@@ -0,0 +1,22 @@
+package network.meikai.mc.uuzselfplaybot;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class GlobalVars {
+
+    // app info
+    public static final String VERSION = "$project.version";
+    public static final String GIT_BRANCH = "$CI_COMMIT_REF_NAME";
+    public static final String GIT_REVERSION = "$CI_COMMIT_SHORT_SHA";
+    public static final String BUILD = "$CI_JOB_ID";
+
+    // server connect
+    public static final String HOST = "127.0.0.1";
+    public static final int PORT = 25565;
+    public static final String BOTNAME = "DreamyYuyuko233";
+
+    // Classes
+    public static final Logger LOGGER = LogManager.getLogger("YuyukoBot #" + BOTNAME + "");
+
+}
--- src/main/java/network/meikai/mc/uuzselfplaybot/YuyukoSelfPlayBot.java
+++ src/main/java/network/meikai/mc/uuzselfplaybot/YuyukoSelfPlayBot.java
@@ -1,19 +1,22 @@
 package network.meikai.mc.uuzselfplaybot;
 
+import com.github.tomaslanger.chalk.Chalk;
+
 public class YuyukoSelfPlayBot {
 //    private static final String HOST = "218.93.206.47";
 //    private static final int PORT = 2705;
 
     public static void main(String[] arg) {
+        Chalk.setColorEnabled(true);
 
-        System.out.println("__   __                 _         ____   ___ _____\n" +
+        System.out.println(Chalk.on("__   __                 _         ____   ___ _____\n" +
                 "\\ \\ / /   _ _   _ _   _| | _____ | __ ) / _ \\_   _|\n" +
                 " \\ V / | | | | | | | | | |/ / _ \\|  _ \\| | | || |\n" +
                 "  | || |_| | |_| | |_| |   < (_) | |_) | |_| || |\n" +
                 "  |_| \\__,_|\\__, |\\__,_|_|\\_\\___/|____/ \\___/ |_|\n" +
-                "            |___/\n");
+                "            |___/\n").green());
 
-        System.out.println();
+        System.out.println("[+] Version: " + GlobalVars.VERSION + "(" + GlobalVars.GIT_BRANCH + ") Git-" + GlobalVars.GIT_REVERSION + " Build: " + GlobalVars.BUILD);
 
     }
 

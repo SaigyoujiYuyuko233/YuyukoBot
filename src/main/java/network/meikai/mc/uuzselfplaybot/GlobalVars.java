db0a77fb8d2c4607c16df0c9bf28a0ba5d9fd2cd
SaigyoujiYuyuko <HGK-SaigyoujiYuyuko@outlook.com>
2020-04-02 10:33:19 +0800

    测试版本号

--- src/main/java/network/meikai/mc/uuzselfplaybot/GlobalVars.java
+++ src/main/java/network/meikai/mc/uuzselfplaybot/GlobalVars.java
@@ -6,10 +6,10 @@ import org.apache.logging.log4j.Logger;
 public class GlobalVars {
 
     // app info
-    public static final String VERSION = "$CI_COMMIT_TAG";
-    public static final String GIT_BRANCH = "$CI_COMMIT_REF_NAME";
-    public static final String GIT_REVERSION = "$CI_COMMIT_SHORT_SHA";
-    public static final String BUILD = "$CI_JOB_ID";
+    public static final String VERSION = "0.1-SNAPSHOT";
+    public static final String GIT_BRANCH = "${CI_COMMIT_REF_NAME}";
+    public static final String GIT_REVERSION = "${CI_COMMIT_SHORT_SHA}";
+    public static final String BUILD = "${CI_PIPELINE_ID}";
 
     // server connect
     public static final String HOST = "127.0.0.1";

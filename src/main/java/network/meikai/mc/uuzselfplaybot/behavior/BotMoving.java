fa292b4680c80dad403ba7d184dc02868ef0e9e2
SaigyoujiYuyuko <HGK-SaigyoujiYuyuko@outlook.com>
2020-04-02 18:14:56 +0800

    修复玩家移动出错

--- src/main/java/network/meikai/mc/uuzselfplaybot/behavior/BotMoving.java
+++ src/main/java/network/meikai/mc/uuzselfplaybot/behavior/BotMoving.java
@@ -6,22 +6,19 @@ import network.meikai.mc.uuzselfplaybot.GlobalVars;
 public class BotMoving {
 
     public void moveToPosition(final Double dstX, Double dstY, final Double dstZ) {
-        Thread moving = new Thread(new Runnable() {
+        Thread movingX = new Thread(new Runnable() {
             @Override
             public void run() {
 
-                // 计算2点之间的距离
+                // 间距
                 double changingX = Math.abs(Math.abs(dstX) - Math.abs(GlobalVars.BotX));
-                double changingZ = Math.abs(Math.abs(dstZ) - Math.abs(GlobalVars.BotZ));
-
-                double distance = Math.sqrt( Math.pow(changingX, 2) + Math.pow(changingZ, 2) );
 
                 // 算出需要执行多少次
-                int steps = (int) Math.round(distance / 0.215);
+                int stepsX = (int) Math.round(changingX / 0.215);
 
-                for ( int i = 0; i < steps; i++ ) {
-                    GlobalVars.BotX += 0.215;
-                    GlobalVars.BotZ += 0.215;
+                for ( int i = 0; i < stepsX; i++ ) {
+                    if (dstX > GlobalVars.BotX) GlobalVars.BotX += 0.215;
+                    if (dstX < GlobalVars.BotX) GlobalVars.BotX -= 0.215;
 
                     GlobalVars.CLIENT.getSession().send(new ClientPlayerPositionPacket(
                         true,
@@ -40,10 +37,47 @@ public class BotMoving {
             }
         });
 
+        Thread movingZ = new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                // 间距
+                double changingZ = Math.abs(Math.abs(dstZ) - Math.abs(GlobalVars.BotZ));
+
+                // 算出需要执行多少次
+                int stepsZ = (int) Math.round(changingZ / 0.215);
+
+                for ( int i = 0; i < stepsZ; i++ ) {
+                    if (dstZ > GlobalVars.BotZ) GlobalVars.BotZ += 0.215;
+                    if (dstZ < GlobalVars.BotZ) GlobalVars.BotZ -= 0.215;
+
+                    GlobalVars.CLIENT.getSession().send(new ClientPlayerPositionPacket(
+                            true,
+                            GlobalVars.BotX,
+                            GlobalVars.BotY,
+                            GlobalVars.BotZ
+                    ));
+
+                    try {
+                        Thread.sleep(60L);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+        });
+
         if ( GlobalVars.canMove ) {
-            moving.setName("PlayerMoving");
-            moving.setDaemon(true);
-            moving.start();
+            movingX.setName("PlayerMoving-X");
+            movingX.setDaemon(true);
+            movingX.start();
+        }
+
+        if ( GlobalVars.canMove ) {
+            movingZ.setName("PlayerMoving-Z");
+            movingZ.setDaemon(true);
+            movingZ.start();
         }
 
     }

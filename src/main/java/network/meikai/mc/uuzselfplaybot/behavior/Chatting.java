932dac87ca5e7981146d67604c350f2166758b00
SaigyoujiYuyuko <HGK-SaigyoujiYuyuko@outlook.com>
2020-04-03 22:43:10 +0800

    jline3 支持输入

--- src/main/java/network/meikai/mc/uuzselfplaybot/behavior/Chatting.java
+++ src/main/java/network/meikai/mc/uuzselfplaybot/behavior/Chatting.java
@@ -2,21 +2,35 @@ package network.meikai.mc.uuzselfplaybot.behavior;
 
 import com.github.steveice10.mc.protocol.packet.ingame.client.ClientChatPacket;
 import network.meikai.mc.uuzselfplaybot.GlobalVars;
+import org.jline.reader.LineReader;
+import org.jline.reader.LineReaderBuilder;
+import org.jline.terminal.Terminal;
+import org.jline.terminal.TerminalBuilder;
+import org.jline.utils.Log;
 
-import java.util.Scanner;
+import java.io.IOException;
 
 public class Chatting implements Runnable{
 
     @Override
     public void run() {
-        Scanner scanner = new Scanner(System.in);
-
         while (true) {
-            String message;
-            
-            message = scanner.nextLine();
+            try {
 
-            GlobalVars.CLIENT.getSession().send(new ClientChatPacket(message));
+                Terminal terminal = TerminalBuilder
+                        .builder()
+                        .dumb(true)
+                        .system(true)
+                        .jansi(true)
+                        .build();
+                LineReader lineReader = LineReaderBuilder.builder().terminal(terminal).build();
+
+                Log.debug(terminal);
+
+                String message = lineReader.readLine("聊天/命令 >> ");
+                GlobalVars.CLIENT.getSession().send(new ClientChatPacket(message));
+
+            } catch (IOException e) { GlobalVars.MAIN_LOGGER.error(e.getStackTrace()); }
         }
     }
 

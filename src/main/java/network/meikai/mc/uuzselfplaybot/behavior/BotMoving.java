50dcfb42a3ecee98ee8606b860ac4a749e62884a
SaigyoujiYuyuko <HGK-SaigyoujiYuyuko@outlook.com>
2020-04-03 12:50:07 +0800

    修正一下yaw

--- src/main/java/network/meikai/mc/uuzselfplaybot/behavior/BotMoving.java
+++ src/main/java/network/meikai/mc/uuzselfplaybot/behavior/BotMoving.java
@@ -1,30 +1,41 @@
 package network.meikai.mc.uuzselfplaybot.behavior;
 
 import com.github.steveice10.mc.protocol.packet.ingame.client.player.ClientPlayerPositionPacket;
+import com.github.steveice10.mc.protocol.packet.ingame.client.player.ClientPlayerPositionRotationPacket;
 import network.meikai.mc.uuzselfplaybot.GlobalVars;
+import network.meikai.mc.uuzselfplaybot.network.EventHandler;
 
 public class BotMoving {
 
     public void moveToPosition(final Double dstX, Double dstY, final Double dstZ) {
+
         Thread movingX = new Thread(new Runnable() {
             @Override
             public void run() {
-
-                // 间距
                 double changingX = Math.abs(Math.abs(dstX) - Math.abs(GlobalVars.BotX));
 
                 // 算出需要执行多少次
-                int stepsX = (int) Math.round(changingX / 0.215);
+                int stepsX = (int) Math.round(changingX / 0.23);
 
                 for ( int i = 0; i < stepsX; i++ ) {
-                    if (dstX > GlobalVars.BotX) GlobalVars.BotX += 0.215;
-                    if (dstX < GlobalVars.BotX) GlobalVars.BotX -= 0.215;
+                    if (dstX > GlobalVars.BotX) GlobalVars.BotX += 0.23;
+                    if (dstX < GlobalVars.BotX) GlobalVars.BotX -= 0.23;
 
-                    GlobalVars.CLIENT.getSession().send(new ClientPlayerPositionPacket(
+                    // yaw
+                    GlobalVars.BotYaw = (float) Math.atan2(
+                            GlobalVars.BotX - dstX,
+                            GlobalVars.BotZ - dstZ
+                    ) + 10;
+
+                    EventHandler.EVENT_LOGGER.debug("Yaw: " + GlobalVars.BotYaw);
+
+                    GlobalVars.CLIENT.getSession().send(new ClientPlayerPositionRotationPacket(
                         true,
                         GlobalVars.BotX,
                         GlobalVars.BotY,
-                        GlobalVars.BotZ
+                        GlobalVars.BotZ,
+                        GlobalVars.BotYaw,
+                        GlobalVars.BotPitch
                     ));
 
                     try {
@@ -41,15 +52,14 @@ public class BotMoving {
             @Override
             public void run() {
 
-                // 间距
                 double changingZ = Math.abs(Math.abs(dstZ) - Math.abs(GlobalVars.BotZ));
 
                 // 算出需要执行多少次
-                int stepsZ = (int) Math.round(changingZ / 0.215);
+                int stepsZ = (int) Math.round(changingZ / 0.23);
 
                 for ( int i = 0; i < stepsZ; i++ ) {
-                    if (dstZ > GlobalVars.BotZ) GlobalVars.BotZ += 0.215;
-                    if (dstZ < GlobalVars.BotZ) GlobalVars.BotZ -= 0.215;
+                    if (dstZ > GlobalVars.BotZ) GlobalVars.BotZ += 0.23;
+                    if (dstZ < GlobalVars.BotZ) GlobalVars.BotZ -= 0.23;
 
                     GlobalVars.CLIENT.getSession().send(new ClientPlayerPositionPacket(
                             true,
